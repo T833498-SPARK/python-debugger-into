1a8e375433278ee2241312e50c8e4f04abe41429
Richard Kyle <richard.kyel@spark.co.nz>
2024-06-11 21:07:11 +0000

    commit

---
+++ demo/justmycode.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data_dict = {
+    "name": ["John", "Tom", "Harry"],
+    "position": ["MLE", "Data Analyst", "Data Scientist"],
+    "age": [30, 25, 35]
+}
+
+pandas_df = pd.DataFrame(data_dict)
